f2e37a240a1e33486038747b3d4ad0408c030263
rkolle <robert.kolle@smail.inf.h-brs.de>
2011-06-06 12:16:58 +0200

    added route to login

--- rothy/config/routes.rb
+++ rothy/config/routes.rb
@@ -16,7 +16,7 @@ Rothy::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  match 'login/check' => 'login#check'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
