86eaefaa064efc955ad80c8dad8934e55b6d5c1b
rkolle <robert.kolle@smail.inf.h-brs.de>
2011-06-19 23:05:29 +0200

    diverses

--- rothy/config/routes.rb
+++ rothy/config/routes.rb
@@ -72,7 +72,7 @@ Rothy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "login#login", :as => 'login'
+    root :to => "login#welcome", :as => 'login'
 
   # See how all your routes lay out with "rake routes"
 
