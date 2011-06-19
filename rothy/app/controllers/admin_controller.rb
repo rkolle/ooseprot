7ccd69e21c7a547a73e29d2982ac956746e926d9
rkolle <robert.kolle@smail.inf.h-brs.de>
2011-06-19 20:27:14 +0200

    Changed various things

--- rothy/app/controllers/admin_controller.rb
+++ rothy/app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  skip_before_filter :authorize
+  
   def index
     @amount_of_users = session.count
   end
--- rothy/app/controllers/users_controller.rb
+++ rothy/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_filter :authorize, :only => [:new, :create]
 
   def index
     @users = User.order(:username)
